03e7d31012584108f162dcab953a6d7706013dc0
dumialex32 <dumitrasalexandru26@gmail.com>
2024-10-27 15:30:28 +0100

    Improve card flipping

--- frontend/src/components/PlayerCard.tsx
+++ frontend/src/components/PlayerCard.tsx
@@ -1,26 +1,22 @@
-import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import { LobbyPlayer } from "../types/lobbyPlayersTypes";
 import StatsRow from "./StatsRow";
 import playerPlaceholder from "../assets/player_placeholder.png";
 
-const PlayerCard: React.FC<{ player: LobbyPlayer }> = ({ player }) => {
-  const [isFlipped, setIsFlipped] = useState<boolean>(false);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setIsFlipped((prevState) => !prevState);
-  };
+const PlayerCard: React.FC<{
+  player: LobbyPlayer;
+  isFlipped: boolean;
+  onFlip: () => void;
+}> = ({ player, isFlipped, onFlip }) => {
   return (
     <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
       <div className="flex flex-col gap-2 shadow-sm shadow-white rounded-md text-white">
         <img src={playerPlaceholder} className="w-full h-full object-contain" />
-
         <div className="flex flex-col gap-2 p-4">
           <h2 className="text-xl overflow-hidden whitespace-nowrap text-ellipsis">
             {player.playerName}
           </h2>
-          <button className="btn" onClick={handleClick}>
+          <button className="btn" onClick={onFlip}>
             Flip for stats
           </button>
         </div>
@@ -28,9 +24,12 @@ const PlayerCard: React.FC<{ player: LobbyPlayer }> = ({ player }) => {
 
       <div className="flex flex-col gap-3 shadow-sm shadow-white rounded-md text-white p-2">
         <div className="flex flex-col gap-2 p-4">
+          <h2 className="text-xl overflow-hidden whitespace-nowrap text-ellipsis">
+            {player.playerName}
+          </h2>
           <StatsRow player={player} />
         </div>
-        <button className="btn" onClick={handleClick}>
+        <button className="btn" onClick={onFlip}>
           Flip back
         </button>
       </div>
--- frontend/src/components/lobbyputernik/lobbyscreens/LobbyPlayersScreen.tsx
+++ frontend/src/components/lobbyputernik/lobbyscreens/LobbyPlayersScreen.tsx
@@ -4,24 +4,42 @@ import axios from "axios";
 import { LOBBYPLAYERS_URL } from "../../../../constants";
 import { LobbyPlayer } from "../../../types/lobbyPlayersTypes";
 import { useLobby } from "../../../hooks/useLobbyContext";
-import PlayerCard2 from "../../PlayerCard";
 
 const LobbyPlayersScreen: React.FC = () => {
   const [lobbyPlayers, setLobbyPlayers] = useState<LobbyPlayer[]>([]);
   const { refetchKey } = useLobby();
+  const [flippedStates, setFlippedStates] = useState<{
+    [key: string]: boolean;
+  }>({});
+  const [areAllFlipped, setAreAllFlipped] = useState<boolean>(false);
+
+  const handleFlipAll = () => {
+    const newFlippedStates = Object.fromEntries(
+      lobbyPlayers.map((player) => [player.steamId, !areAllFlipped])
+    );
+    setFlippedStates(newFlippedStates);
+    setAreAllFlipped((prev) => !prev);
+  };
+
+  const handleSingleFlip = (steamId: string) => {
+    setFlippedStates((prevState) => ({
+      ...prevState,
+      [steamId]: !prevState[steamId],
+    }));
+  };
 
   const getLobbyPlayers = async () => {
     try {
       const res = await axios.get(LOBBYPLAYERS_URL);
-      console.log(res);
       setLobbyPlayers(res.data);
+
+      // Initialize flipped state for all players to false when new data is fetched
+      const initialFlippedStates = Object.fromEntries(
+        res.data.map((player: LobbyPlayer) => [player.steamId, false])
+      );
+      setFlippedStates(initialFlippedStates);
     } catch (err: any) {
-      console.log(err);
-      if (axios.isAxiosError(err)) {
-        console.log(err.response?.data.message);
-      } else {
-        console.error(err.message || "Lobby players data fetch failed");
-      }
+      console.error(err.message || "Lobby players data fetch failed");
     }
   };
 
@@ -30,10 +48,20 @@ const LobbyPlayersScreen: React.FC = () => {
   }, [refetchKey]);
 
   return (
-    <div className="grid grid-cols-8 gap-3">
-      {lobbyPlayers.map((player) => {
-        return <PlayerCard key={player.steamId} player={player} />;
-      })}
+    <div className="flex flex-col gap-3 justify-start">
+      <button className="btn self-start" onClick={handleFlipAll}>
+        Flip All
+      </button>
+      <div className="grid grid-cols-8 gap-3">
+        {lobbyPlayers.map((player) => (
+          <PlayerCard
+            key={player.steamId}
+            player={player}
+            isFlipped={flippedStates[player.steamId] || false}
+            onFlip={() => handleSingleFlip(player.steamId)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
